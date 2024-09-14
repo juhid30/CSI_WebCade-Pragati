8a290992a4bc0b479c9e888587711a23305decd5
jashrashne <jash.rashne@gmail.com>
2024-09-14 08:36:26 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import './index.css'; // Import tailwind styles
-
-import "./App.css";
 import Login from "./components/Login";
 
 function App() {
