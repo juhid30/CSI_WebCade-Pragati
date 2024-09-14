72bcc86699e30d47ae3e1e3049342430a103e725
awwyushh <awwwyushh@gmail.com>
2024-09-14 13:01:21 +0530

    cccc

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import App from "./App.jsx";
 import "./index.css";
 import { ChakraProvider } from "@chakra-ui/react";
@@ -15,5 +14,5 @@ createRoot(document.getElementById("root")).render(
       <AnimCursor/>
         <App />
       </ChakraProvider>
-    </React.StrictMode>
+  </React.StrictMode>
 );
