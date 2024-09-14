2c2f11807e6fc0c5f05c1effeadc216694000c2d
juhid30 <55.juhideore@gmail.com>
2024-09-14 12:59:40 +0530

    Fix

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,15 @@ import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import App from "./App.jsx";
 import "./index.css";
-import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../src/components/CodeEditor/theme.js";
-
+import AnimCursor from "./anim/AnimCursor";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      <BrowserRouter/>
-      <AnimCursor/>
-        <App />
-      </ChakraProvider>
-    </React.StrictMode>
+      <BrowserRouter />
+      <AnimCursor />
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
 );
