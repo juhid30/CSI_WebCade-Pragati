cddf448eaeb890cefee05d1b81e549d966f88557
jashrashne <jash.rashne@gmail.com>
2024-09-14 12:03:13 +0530

    stash

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,18 @@
-import { StrictMode } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import App from "./App.jsx";
 import "./index.css";
+import theme from "../src/components/CodeEditor/theme.js"
+
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
+  <React.StrictMode>
+    <ChakraProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ChakraProvider>
+  </React.StrictMode>
 );
