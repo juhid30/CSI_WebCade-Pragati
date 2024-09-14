053c5f99200b28e9db0e66937d7367c24a102d9f
Nishant0121 <142099904+Nishant0121@users.noreply.github.com>
2024-09-14 10:43:13 +0530

    chatbot for roadmap

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,31 @@ import "./index.css"; // Import tailwind styles
 import "./App.css";
 import Login from "./components/Login";
 import Navbar from "./components/Navbar";
+import CardsIntrest from "./components/CardsIntrest";
 
 function App() {
-  return(
+  return (
     <>
-      <Navbar/>
+      <Navbar />
       <Login />
+      {/* Open the modal using document.getElementById('ID').showModal() method */}
+      <button
+        className="btn"
+        onClick={() => document.getElementById("cards_interest").showModal()}
+      >
+        Chat Bot
+      </button>
+      <dialog id="cards_interest" className="modal">
+        <div className="modal-box bg-white ">
+          <div className="modal-action block">
+            <CardsIntrest />
+            <form method="dialog">
+              {/* if there is a button in form, it will close the modal */}
+              <button className="btn">Close</button>
+            </form>
+          </div>
+        </div>
+      </dialog>
     </>
   );
 }
