1644ada81ee3832645273da5fc1714e773649dc3
awwyushh <awwwyushh@gmail.com>
2024-09-14 09:34:12 +0530

    v3.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,35 @@
-import { useState } from "react";
-import reactLogo from "./assets/logo.svg";
+import { useState, useRef } from "react";
+import "./assets/logo.svg";
 import viteLogo from "/vite.svg";
 import "./index.css"; // Import tailwind styles
+import { LocomotiveScrollProvider } from "react-locomotive-scroll";
 
 import "./App.css";
 import Login from "./components/Login";
 import Navbar from "./components/Navbar";
 
 function App() {
+  const containerRef = useRef(null);
   return(
     <>
-      <Navbar/>
-      <Login />
+      <LocomotiveScrollProvider
+      options={
+        {
+          smooth:true,
+        }
+      }
+      watch={[]}
+      containerRef={containerRef}>
+
+
+        <main data-scroll-container ref={containerRef}>
+          <Navbar/>
+          <Login/>
+          <Login/>
+          <Login/>
+        </main>
+
+      </LocomotiveScrollProvider>
     </>
   );
 }
